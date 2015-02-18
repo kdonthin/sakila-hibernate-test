28ffff031512d5a00734f3c6bd68629e393cb546
Kishore Donthineni <kdonthineni@gmail.com>
2015-02-18 13:57:52 -0500

    Initial Version with third party jars included.

---
+++ SakilaHibernateTest/src/com/sakila/tools/TestApp.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.sakila.tools ;
+
+import org.slf4j.Logger ;
+import org.slf4j.LoggerFactory ;
+
+/**
+ * @author Kishore Donthineni
+ *
+ */
+public class TestApp
+{
+	/**
+	 * @param args
+	 */
+
+	private static Logger logger = LoggerFactory.getLogger(TestApp.class) ;
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		logger.info("Hello, World!") ;
+	}
+}
